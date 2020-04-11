c8d0c57f2d4aa2fdb0745e504f9e51f11dbb91b8
g-mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-04-11 13:23:28 +0100

    Created an app that generates an array of ints and calculates their sum

---
+++ RandomNuimbersPrinter/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNuimbersPrinter
+{
+    /*
+     * Generates an array of random ints with a user declared capacity
+     * and calculates the sum of all elements of that array
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                int capacity;
+
+                while(true)
+                {
+                    Console.WriteLine("Enter an array capacity: ");
+                    if (int.TryParse(Console.ReadLine(), out capacity)) break;
+                    Console.WriteLine("Error! Invalid entry, please try again..");
+                }
+
+                int[] numbers = new int[capacity];
+
+                PopulateArray(numbers);
+                Console.WriteLine();
+                CalculateSum(numbers);
+
+                Console.WriteLine("Press Any Key to restart or Esc to quit..");
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+
+        }
+
+        public static void PopulateArray(int[] numbers)
+        {
+            Random r = new Random();
+
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = r.Next(1, 101);
+                Console.WriteLine($"Element {i + 1} is {numbers[i]}");
+            }
+        }
+
+        public static void CalculateSum(int[] numbers)
+        {
+            long sum = 0;
+
+            foreach(int i in numbers)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine($"The sum of all elements is {sum}");
+        }
+    }
+}
